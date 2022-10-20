70c019e64ec99f772edc913de9ecc66b280c1643
Tobias Brunner <tobias.brunner@vshn.ch>
2022-10-20 14:12:42 +0200

    Initial port and adapt

--- main.go
+++ main.go
@@ -1,109 +1,91 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"io"
+	"net"
 	"os"
-	"os/signal"
-	"sync/atomic"
-	"syscall"
-	"time"
 
-	"github.com/go-logr/logr"
-	"github.com/urfave/cli/v2"
-)
-
-var (
-	// these variables are populated by Goreleaser when releasing
-	version = "unknown"
-	commit  = "-dirty-"
-	date    = time.Now().Format("2006-01-02")
-
-	// TODO: Adjust app name
-	appName     = "go-bootstrap"
-	appLongName = "a generic bootstrapping project"
-
-	// TODO: Adjust or clear env var prefix
-	// envPrefix is the global prefix to use for the keys in environment variables.
-	// Include a delimiter like `_` if required.
-	envPrefix = "BOOTSTRAP_"
+	"github.com/micro/cli/v2"
+	"golang.org/x/net/proxy"
 )
 
 func main() {
-	ctx, stop, app := newApp()
-	defer stop()
-	err := app.RunContext(ctx, os.Args)
-	// If required flags aren't set, it will return with error before we could set up logging
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
-	}
-}
+	app := cli.NewApp()
 
-func newApp() (context.Context, context.CancelFunc, *cli.App) {
-	logInstance := &atomic.Value{}
-	logInstance.Store(logr.Discard())
-	app := &cli.App{
-		Name:    appName,
-		Usage:   appLongName,
-		Version: fmt.Sprintf("%s, revision=%s, date=%s", version, commit, date),
+	app.Usage = "Connect TCP over SOCKS5"
 
-		EnableBashCompletion: true,
-
-		Before: setupLogging,
-		Flags: []cli.Flag{
-			&cli.BoolFlag{
-				Name:    "debug",
-				Aliases: []string{"verbose", "d"},
-				Usage:   "sets the log level to debug",
-				EnvVars: envVars("DEBUG"),
-			},
-			&cli.StringFlag{
-				Name:        "log-format",
-				Usage:       "sets the log format (values: [json, console])",
-				EnvVars:     envVars("LOG_FORMAT"),
-				DefaultText: "console",
-			},
+	app.Flags = []cli.Flag{
+		&cli.IntFlag{
+			Name:    "port",
+			Aliases: []string{"p"},
+			Usage:   "local listen port",
+			Value:   12345,
 		},
-		Commands: []*cli.Command{
-			newExampleCommand(),
+		&cli.StringFlag{
+			Name:    "socks5",
+			Aliases: []string{"x"},
+			Usage:   "socks5 server",
+			Value:   "127.0.0.1:1080",
 		},
-		ExitErrHandler: func(ctx *cli.Context, err error) {
-			if err != nil {
-				AppLogger(ctx).Error(err, "fatal error")
-				cli.HandleExitCoder(cli.Exit("", 1))
-			}
+		&cli.StringFlag{
+			Name:     "target",
+			Aliases:  []string{"t"},
+			Usage:    "target address including port",
+			Required: true,
 		},
 	}
-	hasSubcommands := len(app.Commands) > 0
-	app.Action = rootAction(hasSubcommands)
-	// There is logr.NewContext(...) which returns a context that carries the logger instance.
-	// However, since we are configuring and replacing this logger after starting up and parsing the flags,
-	// we'll store a thread-safe atomic reference.
-	parentCtx := context.WithValue(context.Background(), loggerContextKey{}, logInstance)
-	ctx, stop := signal.NotifyContext(parentCtx, syscall.SIGINT, syscall.SIGTERM)
-	return ctx, stop, app
-}
 
-func rootAction(hasSubcommands bool) func(context *cli.Context) error {
-	return func(ctx *cli.Context) error {
-		if hasSubcommands {
-			return cli.ShowAppHelp(ctx)
+	app.Action = func(c *cli.Context) error {
+		port := c.Int("port")
+		socks5 := c.String("socks5")
+		target := c.String("target")
+
+		addr := fmt.Sprintf(":%d", port)
+		l, err := net.Listen("tcp", addr)
+		if err != nil {
+			fmt.Printf("listen fail: %v\n", err)
+			return err
 		}
-		return LogMetadata(ctx)
+
+		socks5dialer, err := proxy.SOCKS5("tcp", socks5, nil, nil)
+		if err != nil {
+			fmt.Printf("new socks5 dialer fail: %v\n", err)
+			return err
+		}
+
+		fmt.Printf("listen address: '%v'\n", addr)
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				fmt.Printf("accept fail: %v\n", err)
+				continue
+			}
+
+			go handleConnection(socks5dialer.Dial, conn, target)
+		}
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Printf("run fail: %v", err)
 	}
 }
 
-// env combines envPrefix with given suffix delimited by underscore.
-func env(suffix string) string {
-	return envPrefix + suffix
-}
+func handleConnection(dialer func(network, address string) (net.Conn, error), conn net.Conn, addr string) {
+	defer conn.Close()
+	fmt.Printf("new connection %s -> %s\n", conn.RemoteAddr().String(), addr)
 
-// envVars combines envPrefix with each given suffix delimited by underscore.
-func envVars(suffixes ...string) []string {
-	arr := make([]string, len(suffixes))
-	for i := range suffixes {
-		arr[i] = env(suffixes[i])
+	remote, err := dialer("tcp", addr)
+	if err != nil {
+		fmt.Printf("dial %s fail: %v\n", addr, err)
+		return
 	}
-	return arr
+	defer remote.Close()
+	fmt.Printf("connect proxy %s ok\n", addr)
+
+	go io.Copy(conn, remote)
+	io.Copy(remote, conn)
+
+	fmt.Printf("connection %s -> %s closed\n", conn.RemoteAddr().String(), addr)
 }
